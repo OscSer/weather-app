e30b2e1a135612abe714cf9b246e1ebb5b600258
Oscar Serna <oscr.serna@gmail.com>
2021-05-27 09:38:18 -0500

    refactor: :art: refactored card and initial styles added

--- src/App.js
+++ src/App.js
@@ -9,32 +9,31 @@ export default function App() {
   const [long, setLong] = useState([]);
   const [data, setData] = useState([]);
 
-  const fetchData = async () => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
-
-    await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-      .then(res => res.json())
-      .then(result => {
-        setData(result)
-      });
-  }
-
   useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+        .then(res => res.json())
+        .then(result => {
+          setData(result)
+        });
+    }
     fetchData();
   }, [lat, long])
 
   return (
-    <div className="App">
+    <div className="App center">
       {
         (typeof data.main != 'undefined')
           ? <WeatherCard weatherData={data} />
           : (
             <div>
               <Dimmer active>
-                <Loader>Loading..</Loader>
+                <Loader>Loading...</Loader>
               </Dimmer>
             </div>
           )
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Card } from 'semantic-ui-react'
 import moment from 'moment';
+import './WeatherCard.css';
+
+const CardInfo = ({ description, value }) => {
+    return (
+        <div className='card__info'>
+            <span>{`${description}:`}</span>
+            <span className='card__info__value'>{value}</span>
+        </div>
+    )
+}
 
 const WeatherCard = ({ weatherData }) => (
-    <Card>
+    <Card className='card'>
+        <Card.Header className="card__header center">City: {weatherData.name}</Card.Header>
         <Card.Content>
-            <Card.Header className="header">City Name: {weatherData.name}</Card.Header>
-            <p>Temprature: {weatherData.main.temp} &deg;C</p>
-            <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-            <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-            <p>Description: {weatherData.weather[0].main}</p>
-            <p>Humidity: {weatherData.main.humidity} %</p>
-            <p>Day: {moment().format('dddd')}</p>
-            <p>Date: {moment().format('LL')}</p>
+            <CardInfo description='Day' value={moment().format('dddd')} />
+            <CardInfo description='Date' value={moment().format('LL')} />
+            <CardInfo description='Temprature' value={`${weatherData.main.temp} °C`} />
+            <CardInfo description='Sunrise' value={new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')} />
+            <CardInfo description='Sunset' value={new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')} />
+            <CardInfo description='Description' value={weatherData.weather[0].main} />
+            <CardInfo description='Humidity' value={weatherData.main.humidity} />
         </Card.Content>
     </Card>
 )
