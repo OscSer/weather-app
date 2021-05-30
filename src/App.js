8e69991340c0abf7f5cbdae8d82cc44dc27e3e1d
Oscar Serna <oscr.serna@gmail.com>
2021-05-30 11:32:01 -0500

    feat: :sparkles: added refresh button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
 import WeatherCard from './components/WeatherCard';
-import { Search, Container } from 'semantic-ui-react';
+import { Search, Container, Button } from 'semantic-ui-react';
 import cities from './utils/city.list.min.json';
 import { filter, escapeRegExp, uniqBy } from 'lodash';
 
 export default function App() {
   const [coords, setCoords] = useState([]);
   const [results, setResults] = useState([]);
-  const [value, setValue] = useState('Try more cities...');
+  const [value, setValue] = useState('');
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -19,9 +19,7 @@ export default function App() {
     });
   }, []);
 
-  const clearSearch = () => {
-    setValue('');
-  }
+  const clearSearch = () => setValue('');
 
   const onSearchChange = (e, data) => {
     setValue(data.value);
@@ -29,10 +27,13 @@ export default function App() {
     const isMatch = (city) => regExp.test(city.name);
     const filteredCities = filter(cities, isMatch)
       .sort((a, b) => a.name.length - b.name.length)
-      .map(city => ({ ...city, title: `${city.name} (${city.country})` }));
+      .map(city => ({
+        ...city,
+        title: `${city.name} (${city.country})`
+      }));
     const uniqCities = uniqBy(filteredCities, 'title')
       .filter(city => !coords.includes(city.coord));
-    setResults(uniqCities.splice(0, 10));
+    setResults(uniqCities.splice(0, 6));
   }
 
   const onResultSelect = (e, data) => {
@@ -40,26 +41,47 @@ export default function App() {
     clearSearch();
   }
 
+  const onButtonClick = () => {
+    setCoords(coords.map(coord => ({
+      ...coord,
+      key: Math.random().toString(36).slice(2)
+    })));
+  }
+
   return (
-    <div className="App center">
+    <div className="App">
       <Container>
-        <div className="search-container center">
+
+        <div className="search-container">
           <Search
+            input={{ autoFocus: true }}
+            placeholder='Search more cities...'
             onResultSelect={onResultSelect}
             onSearchChange={onSearchChange}
             results={results}
             value={value}
             onFocus={clearSearch}
           />
+          <Button
+            circular
+            primary
+            size='big'
+            className='refresh-button'
+            content='Refresh'
+            icon='refresh'
+            onClick={onButtonClick}
+          />
         </div>
+
         <div className='cards-container'>
           {coords.length > 0 && coords.map(coord => (
             <WeatherCard
-              key={`${coord.lat}-${coord.lon}`}
+              key={`${coord.lat}-${coord.lon}-${coord.key}`}
               coords={coord}
             />
           ))}
         </div>
+
       </Container>
     </div>
   );
